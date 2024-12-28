0c8d1cc2edd01f0f905fa24891e1e3e30cb3ce8e
Yusufrhman <zvistyt@gmail.com>
2024-12-28 15:01:02 +0700

    added header

---
+++ components/header/Header.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import NavLink from "./NavLink";
+
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r from-custom-olive-green via-custom-light-olive-green to-custom-lime-green w-full h-fit rounded-md p-5">
+      <ul className="text-center flex items-center justify-center gap-10">
+        <li>
+          <NavLink href={"/"} className="text-white text-md font-thin">
+            Beranda
+          </NavLink>
+        </li>
+        <li>
+          <NavLink href={"/"} className="text-white text-md font-thin">
+            Produk
+          </NavLink>
+        </li>
+        <li>
+          <Link href={"/"} className="text-white text-3xl font-Grenze-Gotisch">
+            UMKM Desa Kare
+          </Link>
+        </li>
+        <li>
+          <NavLink href={"/"} className="text-white text-md font-thin">
+            Tentang
+          </NavLink>
+        </li>
+        <li>
+          <NavLink href={"/"} className="text-white text-md font-thin">
+            Kontak
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+}
---
+++ components/header/NavLink.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NavLink({
+  children,
+  className,
+  href,
+}: {
+  children: React.ReactNode;
+  className: string;
+  href: string;
+}) {
+  return (
+    <Link href={href} className={`text-white text-md font-thin ${className}`}>
+      {children}
+    </Link>
+  );
+}
