b7c7e7b1004eccca0045e42f2d582d745ddddb6e
Yusufrhman <zvistyt@gmail.com>
2025-01-23 22:20:50 +0700

    added products revalidate

--- lib/database/actions/product.ts
+++ lib/database/actions/product.ts
@@ -1,15 +1,18 @@
 import db from "@/lib/database/db";
+import { cache } from "react";
+import { unstable_cache as nextCache } from "next/cache";
 
-export async function getAllProducts({
-  limit,
-  excludeId,
-}: {
-  limit?: number;
-  excludeId?: string;
-}) {
-  try {
-    // Base query with additional condition to exclude specific product
-    let query = `
+export const getAllProducts = nextCache(
+  cache(async function getAllProducts({
+    limit,
+    excludeId,
+  }: {
+    limit?: number;
+    excludeId?: string;
+  }) {
+    try {
+      // Base query with additional condition to exclude specific product
+      let query = `
      SELECT p.*, 
             GROUP_CONCAT(pi.image_url ORDER BY pi.created_at) AS image_url
      FROM products p
@@ -20,50 +23,54 @@ export async function getAllProducts({
      ORDER BY p.created_at desc
    `;
 
-    // Prepare query parameters
-    const queryParams = excludeId
-      ? [excludeId] // Include excludeId if provided
-      : [];
+      // Prepare query parameters
+      const queryParams = excludeId
+        ? [excludeId] // Include excludeId if provided
+        : [];
 
-    // If limit is provided, modify the query to include it
-    if (limit) {
-      query += ` LIMIT ${limit}`;
+      // If limit is provided, modify the query to include it
+      if (limit) {
+        query += ` LIMIT ${limit}`;
+      }
+
+      // Execute the query and fetch the products
+      const products = (await db.query(query, queryParams)) as any;
+
+      // Ensure the query returns valid data
+      if (!products || products.length === 0) {
+        return []; // Return empty array instead of throwing an error
+      }
+
+      // Transform image_url from a comma-separated string to an array
+      const result = products[0].map((product: any) => {
+        return {
+          ...product,
+          image_url: product.image_url ? product.image_url.split(",") : [], // Convert string to array
+        };
+      });
+
+      return result;
+    } catch (error) {
+      console.error("Error in getAllProducts:", error);
+      throw new Error("An error occurred while fetching the products.");
     }
-
-    // Execute the query and fetch the products
-    const products = (await db.query(query, queryParams)) as any;
-
-    // Ensure the query returns valid data
-    if (!products || products.length === 0) {
-      return []; // Return empty array instead of throwing an error
-    }
-
-    // Transform image_url from a comma-separated string to an array
-    const result = products[0].map((product: any) => {
-      return {
-        ...product,
-        image_url: product.image_url ? product.image_url.split(",") : [], // Convert string to array
-      };
-    });
-
-    return result;
-  } catch (error) {
-    console.error("Error in getAllProducts:", error);
-    throw new Error("An error occurred while fetching the products.");
-  }
-}
-export async function getAllProductByUMKMid({
-  umkmId,
-  limit,
-  excludeId,
-}: {
-  umkmId: string;
-  limit?: number;
-  excludeId?: string;
-}) {
-  try {
-    // Base query with additional condition to exclude specific product
-    let query = `
+  }),
+  ["products"],
+  { revalidate: 60 }
+);
+export const getAllProductByUMKMid = nextCache(
+  cache(async function getAllProductByUMKMid({
+    umkmId,
+    limit,
+    excludeId,
+  }: {
+    umkmId: string;
+    limit?: number;
+    excludeId?: string;
+  }) {
+    try {
+      // Base query with additional condition to exclude specific product
+      let query = `
       SELECT p.*, 
              GROUP_CONCAT(pi.image_url ORDER BY pi.created_at) AS image_url
       FROM products p
@@ -74,38 +81,40 @@ export async function getAllProductByUMKMid({
       ORDER BY p.created_at desc
     `;
 
-    // Prepare query parameters
-    const queryParams = excludeId
-      ? [umkmId, excludeId] // Include excludeId if provided
-      : [umkmId];
+      // Prepare query parameters
+      const queryParams = excludeId
+        ? [umkmId, excludeId] // Include excludeId if provided
+        : [umkmId];
 
-    // If limit is provided, modify the query to include it
-    if (limit) {
-      query += ` LIMIT ${limit}`;
+      // If limit is provided, modify the query to include it
+      if (limit) {
+        query += ` LIMIT ${limit}`;
+      }
+
+      // Execute the query and fetch the products
+      const products = (await db.query(query, queryParams)) as any;
+
+      // Ensure the query returns valid data
+      if (!products || products.length === 0) {
+        return []; // Return empty array instead of throwing an error
+      }
+
+      // Transform image_url from a comma-separated string to an array
+      const result = products[0].map((product: any) => {
+        return {
+          ...product,
+          image_url: product.image_url ? product.image_url.split(",") : [], // Convert string to array
+        };
+      });
+
+      return result;
+    } catch (error) {
+      console.error("Error in getAllProductByUMKMid:", error);
+      throw new Error("An error occurred while fetching the products.");
     }
-
-    // Execute the query and fetch the products
-    const products = (await db.query(query, queryParams)) as any;
-
-    // Ensure the query returns valid data
-    if (!products || products.length === 0) {
-      return []; // Return empty array instead of throwing an error
-    }
-
-    // Transform image_url from a comma-separated string to an array
-    const result = products[0].map((product: any) => {
-      return {
-        ...product,
-        image_url: product.image_url ? product.image_url.split(",") : [], // Convert string to array
-      };
-    });
-
-    return result;
-  } catch (error) {
-    console.error("Error in getAllProductByUMKMid:", error);
-    throw new Error("An error occurred while fetching the products.");
-  }
-}
+  }),["productsbyumkm"],
+  { revalidate: 60 }
+);
 
 export async function getProductById(id: string) {
   try {
